6bca12f738853784a9b060a731597cb16e15c27f
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2022-02-27 18:32:05 +0100

    Make header responsive and add menu for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header>
       <div className="header-container-top">
@@ -12,18 +16,35 @@ const Header = () => {
               DEMO Streaming
             </Link>
           </h1>
-          <div className="menu">
-            <Link className="menu-link" to="/series">
-              Series
-            </Link>
-            <Link className="menu-link" to="/movies">
-              Movies
-            </Link>
+          <div
+            className="header-menu"
+            style={{ transform: `${isMenuOpen ? "translateY(0)" : ""}` }}
+          >
+            <div className="menu">
+              <Link className="menu-link" to="/series">
+                Series
+              </Link>
+              <Link className="menu-link" to="/movies">
+                Movies
+              </Link>
+            </div>
+            <div className="login">
+              <Link to="/" className="header-btn-login">
+                Log in
+              </Link>
+              <Link to="/" className="header-btn-trial">
+                Start your free trial
+              </Link>
+            </div>
           </div>
-          <div className="login">
-            <button className="header-btn-login">Log in</button>
-            <button className="header-btn-trial">Start your free trial</button>
-          </div>
+          <button
+            className={`hamburger-icon ${isMenuOpen && "hamburgerOpen"}`}
+            onClick={handleMenu}
+          >
+            <div className="hamburger-icon-line line1"></div>
+            <div className="hamburger-icon-line line2"></div>
+            <div className="hamburger-icon-line line3"></div>
+          </button>
         </nav>
       </div>
     </header>
